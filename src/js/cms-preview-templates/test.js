d354710dea1d78c65e4e2c67ac986a3be4fcad4a
Philipp Hamerle <phamerle.mmt-b2016@fh-salzburg.ac.at>
2018-01-11 15:30:57 +0100

    da fuck

--- src/js/cms-preview-templates/test.js
+++ src/js/cms-preview-templates/test.js
@@ -1,27 +1,27 @@
 import React from "react";
 
-const ContactEntry = ({heading, text}) =>
+const TestEntry = ({heading, text}) =>
   <div>
     <h4 className="f4 b lh-title mb2 primary">{ heading }</h4>
     <p>{ text }</p>
   </div>;
 
-const ContactEntries = ({data}) => data && data.length > 0
+const TestEntries = ({data}) => data && data.length > 0
     ? <div className="flex-ns mb3">
-      {data.map(({heading, text}) => <ContactEntry heading={heading} text={text} />)}
+      {data.map(({heading, text}) => <TestEntry heading={heading} text={text} />)}
     </div>
     : "";
 
-export default class ContactPreview extends React.Component {
+export default class TestPreview extends React.Component {
   render() {
     const {entry, getAsset, widgetFor} = this.props;
-    const entryContactEntries = entry.getIn(["data", "contact_entries"]);
-    const contactEntries = entryContactEntries ? entryContactEntries.toJS() : [];
+    const entryTestEntries = entry.getIn(["data", "test_entries"]);
+    const testEntries = entryTestEntries ? entryTestEntries.toJS() : [];
     return <div className="ph3 bg-off-white">
       <img src={getAsset(entry.getIn(["data", "logo"]))} alt="" className="db w4 center pv4" />
       <div className="center mw6 pv3">
         { widgetFor("body") }
-        <ContactEntries data={contactEntries} />
+        <TestEntries data={testEntries} />
       </div>
     </div>;
   }
